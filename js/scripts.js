d137ad3f8efe441470ac6b755dbb61ab7f168a69
Sierra Kruisselbrink <sierra.kruisselbrink@uleth.ca>
2024-11-19 16:49:35 -0700

    worked with javascript and jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
 console.log("Hello, World!");
 
 console.log("Hello");
+
+$("#num-19").append("<h2>World Toilet Day</h2>")
+$("#num-19").css("background-image", "url(imgs/wtd.webp)")
+$("#num-19").css("background-size", "cover")
+$("#num-19").css("color", "white")
+
+$("#num-20").addClass("wcd")
+$("#num-20").append("<h2>World Children's Day</h2>")
+
+$("#num-21").addClass("tvd")
+$("#num-21").append("<h2>World Television Day</h2>")
+$("#num-21").css("background-image", "url(imgs/tv.jpeg)")
+$("#num-21").css("background-size", "cover")
+
+$("#num-25").append("<h2>International Day for the Elimination of Violence against Women</h2>")
+$("#num-25").css("background-image","url(imgs/no-excuse.png)")
+$("#num-25").append("<a href='https://www.un.org/en/observances/ending-violence-against-women-day'> Learn More </a>")
